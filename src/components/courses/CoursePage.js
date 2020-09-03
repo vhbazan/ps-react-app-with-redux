699731e1c9fee10802321f11a4aba2a21923ddba
vbazanm <victor_bazan_milla@optum.com>
2020-09-03 14:01:22 +0100

    Add basic course form, course state and event handler

--- src/components/courses/CoursePage.js
+++ src/components/courses/CoursePage.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 
 class CoursePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: ""
+      }
+    };
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  }
+
   render() {
     return (
-      <div>
+      <form>
         <h2>Courses</h2>
-      </div>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
     );
   }
 }
